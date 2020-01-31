9d60ea457ffd0ebcaf0fb6aeae0bd09f6ca17188
Batu Berk Şahin <batuberkshn@gmail.com>
2020-01-31 22:04:38 +0300

    Initial Commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func main() {
+	rl.InitWindow(1600, 900, "raylib [core] example - basic window")
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
