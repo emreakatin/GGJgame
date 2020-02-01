c13be7454f98b9f807e0890a2463927be85ad9c3
Batu Berk Şahin <batuberkshn@gmail.com>
2020-02-01 19:06:46 +0300

    Turret remove and destroying

--- assets/turret.go
+++ assets/turret.go
@@ -2,6 +2,10 @@ package assets
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+const (
+	TurretRadius = 50
+)
+
 type Turret struct {
 	ID uint
 
@@ -21,4 +25,5 @@ var LastTurretID = 0
 
 func (turret Turret) DrawTurret() {
 	rl.DrawTexturePro(turret.Texture, rl.Rectangle{0, 0, float32(turret.Texture.Width), float32(turret.Texture.Height)}, rl.Rectangle{turret.Position.X, turret.Position.Y, float32(turret.Texture.Width), float32(turret.Texture.Height)}, rl.Vector2{float32(turret.Texture.Width / 2), float32(turret.Texture.Height / 2)}, turret.Rotation, rl.White)
+	rl.DrawCircle(int32(turret.Position.X+float32(0)/2), int32(turret.Position.Y+float32(0)/2), TurretRadius, rl.Color{uint8(turret.OwnerID+2) * uint8(40), uint8(turret.OwnerID+1) * uint8(40), uint8(turret.OwnerID+1) * uint8(40), 50})
 }
--- event/turret.go
+++ event/turret.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"strconv"
+
 	"github.com/emreakatin/GGJgame/assets"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -20,7 +22,7 @@ func TurretController() {
 					ID:       uint(assets.LastTurretID),
 					OwnerID:  assets.PlayerID,
 					Position: assets.PlayerPosition,
-					Health:   0,
+					Health:   100,
 					Rotation: assets.PlayerRotation,
 					Texture:  rl.LoadTexture("sprites/turret.png"),
 				}
@@ -28,9 +30,51 @@ func TurretController() {
 				assets.Turrets = append(assets.Turrets, turret)
 			} else if rl.IsKeyPressed(rl.KeyT) && assets.PlayerInventory.MechanicParts < assets.TurretCost {
 				promptFlagMoney = true
+			} else if rl.IsKeyDown(rl.KeyE) {
+				// TURRET COLLIDING
+				for index, turret := range assets.Turrets {
+
+					if rl.CheckCollisionCircleRec(rl.Vector2{(turret.Position.X + float32(0)/2), (turret.Position.Y + float32(0)/2)}, assets.TurretRadius, rl.Rectangle{float32(assets.PlayerPosition.X), float32(assets.PlayerPosition.Y), float32(assets.Player.Width * assets.PlayerScale), float32(assets.Player.Height * assets.PlayerScale)}) {
+						if turret.OwnerID == assets.PlayerID {
+							if turret.Health > 0 {
+								assets.Turrets[index].Health -= 1
+								assets.DrawPrompter("Removing turret. %"+strconv.Itoa(100-int(turret.Health)), 23, 235)
+							} else if turret.Health <= 0 {
+								assets.Turrets = append(assets.Turrets[:index], assets.Turrets[index+1:]...)
+								assets.PlayerInventory.MechanicParts += assets.TurretCost * 2 / 3
+							}
+
+						} else if turret.OwnerID != assets.PlayerID {
+							if turret.Health > 0 && assets.PlayerInventory.MechanicParts >= float32(turret.Health) {
+								assets.Turrets[index].Health -= 1
+								assets.PlayerInventory.MechanicParts -= 15 / 100
+								assets.DrawPrompter("Destroying turret. %"+strconv.Itoa(100-int(turret.Health)), 23, 235)
+							} else if turret.Health <= 0 {
+								assets.Turrets = append(assets.Turrets[:index], assets.Turrets[index+1:]...)
+							}
+						}
+					}
+
+				}
 			}
 		}
 	}
+
+	// TURRET COLLIDING PROMPT
+	for _, turret := range assets.Turrets {
+
+		if rl.CheckCollisionCircleRec(rl.Vector2{(turret.Position.X + float32(0)/2), (turret.Position.Y + float32(0)/2)}, assets.TurretRadius, rl.Rectangle{float32(assets.PlayerPosition.X), float32(assets.PlayerPosition.Y), float32(assets.Player.Width * assets.PlayerScale), float32(assets.Player.Height * assets.PlayerScale)}) {
+			if !rl.IsKeyDown(rl.KeyE) {
+				if turret.OwnerID == assets.PlayerID {
+					assets.DrawPrompter("Hold press \"E\" for remove this turret", 23, 235)
+				} else if turret.OwnerID != assets.PlayerID {
+					assets.DrawPrompter("Hold press \"E\" for destroy this enemy turret", 23, 235)
+				}
+			}
+		}
+
+	}
+
 	// PROMPTER FOR OUT OF SAFE ZONE AND MONEY
 	if promptFlagMoney {
 		promptTickerMoney++
