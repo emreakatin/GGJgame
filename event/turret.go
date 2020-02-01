5d86d5fc09930e52dcabb5e91b9a102090a076e6
Batu Berk Şahin <batuberkshn@gmail.com>
2020-02-01 17:48:59 +0300

    Turret inside on the station

--- event/turret.go
+++ event/turret.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"fmt"
+
 	"github.com/emreakatin/GGJgame/assets"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -9,30 +11,37 @@ var promptTicker = 0
 var promptFlag = false
 
 func TurretController() {
+
 	// CREATE TURRET
+	for _, station := range assets.Stations {
+		if rl.CheckCollisionCircleRec(rl.Vector2{station.Position.X + float32(station.Texture.Width/2), station.Position.Y + float32(station.Texture.Height/2)}, assets.StationRadius, rl.Rectangle{float32(assets.PlayerPosition.X), float32(assets.PlayerPosition.Y), float32(assets.Player.Width * assets.PlayerScale), float32(assets.Player.Height * assets.PlayerScale)}) && station.OwnerID == int(assets.PlayerID) {
+			if rl.IsKeyPressed(rl.KeyT) && assets.PlayerInventory.MechanicParts > assets.TurretCost {
+				assets.LastTurretID += 1
+				assets.PlayerInventory.MechanicParts += -assets.TurretCost
+				turret := assets.Turret{
+					ID:       uint(assets.LastTurretID),
+					OwnerID:  assets.PlayerID,
+					Position: assets.PlayerPosition,
+					Health:   0,
+					Rotation: assets.PlayerRotation,
+					Texture:  rl.LoadTexture("sprites/turret.png"),
+				}
 
-	if rl.IsKeyPressed(rl.KeyT) && assets.PlayerInventory.MechanicParts > assets.TurretCost {
-		assets.LastTurretID += 1
-		assets.PlayerInventory.MechanicParts += -assets.TurretCost
-		turret := assets.Turret{
-			ID:       uint(assets.LastTurretID),
-			OwnerID:  assets.PlayerID,
-			Position: assets.PlayerPosition,
-			Health:   0,
-			Rotation: assets.PlayerRotation,
-			Texture:  rl.LoadTexture("sprites/turret.png"),
+				assets.Turrets = append(assets.Turrets, turret)
+			} else if rl.IsKeyPressed(rl.KeyT) && assets.PlayerInventory.MechanicParts < assets.TurretCost {
+				promptFlag = true
+			}
+		} else {
+			fmt.Println("b")
 		}
-
-		assets.Turrets = append(assets.Turrets, turret)
-	} else if rl.IsKeyPressed(rl.KeyT) && assets.PlayerInventory.MechanicParts < assets.TurretCost {
-		promptFlag = true
 	}
 
+	// PROMPTER FOR MOMEY
 	if promptFlag {
 		promptTicker++
-		if promptTicker < 180 {
+		if promptTicker < 90 {
 			assets.DrawPrompter("You do not have enough money", 23, 250)
-		} else if promptTicker == 180 {
+		} else if promptTicker == 90 {
 			promptFlag = false
 			promptTicker = 0
 		}
